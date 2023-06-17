475dd2539b7f370cb3c3a9c15e804cf51c51dbe0
Gwineafowl727 <tennysonengquist@gmail.com>
2023-06-17 12:40:13 -0700

    finished hit circle density

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import readable
+import numpy as np
+import sys
   
 np.set_printoptions(threshold=sys.maxsize, suppress=True)
 
-print(readable.start_new_map('cycle hit.osu', 0, 0, 0, 0, 1))
+print(readable.start_new_map('cycle hit.osu'))
--- readable.py
+++ readable.py
@@ -1,12 +1,39 @@
-import sys
 import numpy as np
+
+class circle:
+	def __init__(self, timestamp, x, y, euclidean_distance, angle):
+		self.timestamp = timestamp
+		self.x = x
+		self.y = y
+		self.euclidean_distance = euclidean_distance
+		self.angle = angle
+
+class slider:
+	def __init__(self, timestamp_start, timestamp_end, x_start, y_start, x_end, y_end, curve_array, movement, euclidean_distance, angle, reverse):
+		self.timestamp_start = timestamp_start
+		self.timestamp_end = timestamp_end
+		self.x_start = x_start
+		self.y_start = y_start
+		self.x_end = x_end
+		self.y_end = y_end
+		self.curve_array = curve_array
+		self.movement = movement
+		self.euclidean_distance = euclidean_distance
+		self.angle = angle
+		self.reverse = reverse
+
+class curvePoint:
+	def __init__(self, x, y):
+		pass
+	
 
 def get_original_ar(map):
 	for line in map:
-		if line.startswith('ApproachRate'):
-			return line.replace('ApproachRate:', '')
+		if line.startswith("ApproachRate"):
+			return line.replace("ApproachRate:", "")
 
-def ar_to_ms(ar):  # Equations sourced from osu! wiki
+def ar_to_ms(ar):
+	# Equations sourced from osu! wiki
 	if ar == 5:
 		return 1200
 	elif ar < 5:
@@ -14,7 +41,8 @@ def ar_to_ms(ar):  # Equations sourced from osu! wiki
 	else:
 		return 1200 - 750 * (ar - 5) / 5
 
-def ms_to_ar(ms):  # Inverse of the function above, essentially
+def ms_to_ar(ms):
+	# Inverse of the function above, essentially
 	if ms == 1200:
 		return 5
 	elif ms > 1200:
@@ -30,119 +58,35 @@ def ms_to_ar(ms):  # Inverse of the function above, essentially
 		else:
 			return ar
 
-def get_modded_stats(original_ar, EZ, HR, DT, HT):
-	if EZ == 1 and HR == 0 and DT == 0 and HT == 0:  # EZ
-		modded_ar = original_ar * 0.5
-		modded_ms = ar_to_ms(modded_ar)
-		return modded_ar, modded_ms
+def get_distance_factor(x1, y1, x2, y2):
+	# "1" indicates current hitobject, "2" indicates previous.
+	euclidean_distance = np.sqrt((x1 - x2)**2 - (y1 - y2)**2)
+	return ((np.log(euclidean_distance / 10) / 3) + 0.595), euclidean_distance
 
-	elif EZ == 1 and HR == 0 and DT == 1 and HT == 0:  # EZDT
-		modded_ar = original_ar / 2
-		modded_ms = ar_to_ms(modded_ar) * (2 / 3)
-		return ms_to_ar(modded_ms), modded_ms
-
-	elif EZ == 1 and HR == 0 and DT == 0 and HT == 1: # EZHT
-		modded_ar = original_ar * 0.5
-		modded_ms = ar_to_ms(modded_ar) * (4 / 3)
-		return ms_to_ar(modded_ms), modded_ms
-
-	elif EZ == 0 and HR == 1 and DT == 0 and HT == 0:  # HR
-		if original_ar * 1.4 > 10:
-			modded_ar = 10
-			return modded_ar, ar_to_ms(modded_ar)
-		else:
-			modded_ar = original_ar * 1.
-			return modded_ar, ar_to_ms(modded_ar)
-
-	elif EZ == 0 and HR == 1 and DT == 1 and HT == 0:  # HRDT
-		if original_ar * 1.4 > 10:
-			modded_ar = 10
-		else:
-			modded_ar = original_ar * 1.4
-		modded_ms = ar_to_ms(modded_ar) * (2 / 3)
-		return ms_to_ar(modded_ms), modded_ms
-
-	elif EZ == 0 and HR == 1 and DT == 0 and HT == 1:  # HRHT
-		if original_ar * 1.4 > 10:
-			modded_ar = 10
-		else:
-			modded_ar = original_ar * 1.4
-		modded_ms = ar_to_ms(modded_ar) * (4 / 3)
-		return ms_to_ar(modded_ms), modded_ms
-
-	elif EZ == 0 and HR == 0 and DT == 1 and HT == 0:  # DT
-		modded_ms = ar_to_ms(original_ar) * (2 / 3)
-		return ms_to_ar(modded_ms), modded_ms
-
-	elif EZ == 0 and HR == 0 and DT == 0 and HT == 1:  # HT
-		modded_ms = ar_to_ms(original_ar) * (4 / 3)
-		return ms_to_ar(modded_ms), modded_ms
+def get_delta_distance_factor(d1, d2):
+	# "1" indicates current hitobject, "2" indicates previous.
+	difference = d1 - d2
+	if difference > 0:
+		return ((np.log(difference + 170) / 10 ) / 3) + 0.595
 	else:
-		sys.exit('Conflict with mod selection')  # Put something else during app development
+		# for if the difference is negative, i.e. a decelerating stream
+		return ((np.log(difference + 340) / 10 ) / 4) + 0.595
+	
+def get_angle_factor(x1, y1, x2, y2, x3, y3, hitobject_list):
 
-def get_raw_timestamp(line):  # For raw density: each circle and slider will contribute to density equally.
-	s = line.split(',')
-	hitobject_type = int(s[3])
-
-	if hitobject_type == 12:
-		return 'spinner'  # a hit object that is a spinner will not contribute to density.
-
-	else:
-		return int(s[2])  # time
-
-def get_raw_density(map, path_to_map, ms):
-	c = 0  # The variable "c" acts like a line counter.
-	timestamps = np.empty(0, dtype=int)
-	density_array = np.empty(0, dtype=int)
-
-	for line in map:
-		if line.startswith('[HitObjects]'):
-			break
-
-	for line in map:
-		time = get_raw_timestamp(line)
-		if time != 'spinner':
-			timestamps = np.append(timestamps, time)
-
-	circle_amount = (np.count_nonzero(timestamps, axis=None))
-	for i in range(0, circle_amount):  # The variable "i" represents the particular circle being analyzed.
-		circles_on_screen = 1
-		if i == (circle_amount - 1):
-			density_array = np.append(density_array, circles_on_screen)
-		elif timestamps[i + 1] - timestamps[i] > ms:
-			density_array = np.append(density_array, circles_on_screen)
-			continue
-		else:
-			for j in range(1, circle_amount):
-				if i + j == circle_amount:  # Band-aid solution to prevent next elif from checking for index of circle_amount (index is nonexistent), which breaks program.
-					break
-				elif timestamps[i + j] - timestamps[i] < ms:
-					circles_on_screen = circles_on_screen + 1
-				else:
-					break
-			density_array = np.append(density_array, circles_on_screen)
-
-	return np.reshape(np.append(timestamps, density_array, axis=0), newshape=(2, circle_amount))  # Combines time and density arrays into 2d array.
-
-# working on adjusted density!
-def get_orientation(p, coord, coordinates):
-	x1, y1, x2, y2, x3, y3 = coord[0], coord[1], coordinates[p, 0], coordinates[p, 1], coordinates[p, 0], coordinates[p, 1]
-
+	# Trying to understand this again is giving me cancer so please just trust that it works !!
 	slope1 = (y2 - y1) * (x2 - x1)
 	slope2 = (y3 - y2) * (x3 - x2)
 
 	if slope1 > slope2:
-		return 'clockwise'
+		orientation = 'clockwise'
 	elif slope1 < slope2:
-		return 'counterclockwise'
+		orientation = 'counterclockwise'
 	else:
 		if ((x2 <= x1 <= x3) or (x3 <= x1 <= x2)) and ((y2 <= y1 <= y3) or (y3 <= y1 <= y2)):
-			return 'collinear-reverse'
+			oritentation = 'collinear-reverse'
 		else:
-			return 'collinear-straight'
-
-def get_angle(p, coord, coordinates):
-	orientation = get_orientation(p, coord, coordinates)
+			orientation = 'collinear-straight'
 
 	if orientation == 'collinear-straight':
 		angle = 0
@@ -150,11 +94,11 @@ def get_angle(p, coord, coordinates):
 	elif orientation == 'collinear-reverse':
 		angle = 180
 
-	elif orientation == 'clockwise' or 'counterclockwise':
-
-		a = coord
-		b = coordinates[p]
-		c = coordinates[p - 1]
+	else:
+		# Angle between two vectors
+		a = np.array([x1, y1])
+		b = np.array([x2, y2])
+		c = np.array([x3, y3])
 
 		ba = a - b
 		bc = c - b
@@ -168,198 +112,204 @@ def get_angle(p, coord, coordinates):
 		if orientation == 'clockwise':
 			angle = -angle
 
-	return angle
-
-def get_distance(point_1, point_2):
-    squared_distance = np.sum(np.square(point_1 - point_2))
-    return np.sqrt(squared_distance)
-
-def get_distance_diff(p, distance, coordinates):
-	return distance - get_distance(coordinates[p], coordinates[p - 1])
-
-def get_stack_obscurity(p, coord, coordinates, ts, timestamps, ms, hitobjects):
-	obscurity = 1
-
-	for i in range(np.size(timestamps) - 1, -1, -1):
-
-		if (ts - timestamps[i] < ms) and (np.array_equal(coord, coordinates[i])):
-			obscurity = (ts - timestamps[i]) / ms
-
-			if hitobjects[i] == 'slider':
-				obscurity = obscurity / 2
-
-	return obscurity
-
-def get_distance_factor(distance):
-	if distance != 0:
-		return (np.log10(distance / 10)) / 3 + 0.595  # https://www.desmos.com/calculator/q53zdovpss
-	else:
-		return 0.4
-
-def get_distance_diff_factor(distance_diff):
-	if distance_diff > 0:
-		return ((np.log10((distance_diff + 170) / 10)) / 3) + 0.595
-	else:
-		return 1
-
-def get_angle_factor(p, angle, angles):
-	if p == 0:
-		return 1
-	elif np.sign(angle) != np.sign(angles[p]):
-		resulting = np.abs(angle) + np.abs(angles[p])
+	if np.sign(angle) != np.sign(hitobject_list[-1].angle):
+		resulting = np.abs(angle) + np.abs(hitobject_list[-1].angle)
 	else:
 		resulting = np.abs(angle)
 
-	return ((np.sin(resulting / 458.5)) ** 0.34) + 1  # https://www.desmos.com/calculator/ug1xywioa3
+def get_stack_factor(stack_ms, type_current, type_previous):
 
-def get_length_factor(hitobject_type, line_stats):
-	if hitobject_type == 'circle':
-		return 1
-	else:
-		length = float(line_stats[7])
-		return np.cbrt(length / 10000) + 1
+	if type_current == circle:
 
-def get_stack_factor(stack):
-	if stack != 1:
-		return ((-1) * np.log10(stack + 2)) + 1.55
-	else:
-		return 1
+		if type_previous == "circle" or "sliderstart":
+			return np.log10(stack_ms * 5 + 2) + 0.7
+		else:
+			return np.log10(stack_ms + 2) + 0.7
 
-def get_adjusted_hitobject(line, p, ms, hitobjects, timestamps, coordinates, angles):
-	line_stats = line.split(',')
+	if type_current == slider:
+		pass
 
-	hitobject_type = int(line_stats[3])
-    
-	if hitobject_type == 12:  # Rules out inclusion of spinners in data
-		return 'spinner', 'spinner', 'spinner', 'spinner', 'spinner'
 
-	elif bin(hitobject_type).endswith('1'):
-		hitobject_type = 'circle'
-	else:
-		hitobject_type = 'slider'
+def get_object_range(hitobject_list, object_timestamp, map_ms):
 
-	ts = int(line_stats[2])
-	coord = np.array([(line_stats[0]), (line_stats[1])], dtype=int)
+	if len(hitobject_list) == 0:
+		return 0
+	
+	in_view = 0
+	for i in range(len(hitobject_list) - 1, -1, -1):
 
-	distance = 0
-	distance_diff = 0
-	angle = 0
-	stack = 0
+		if type(hitobject_list[i]) == circle:
+			if object_timestamp - hitobject_list[i].timestamp >= map_ms:
+				in_view += 1
 
-	if p == -1:
-		distance_factor = 1
-		distance_diff_factor = 1
-		angle_factor = 1
-		stack_factor = 1
+		else:  # Is basically the same as the if statement above, but accounts for slider attribute name
+			if object_timestamp - hitobject_list[i].timestamp_end >= map_ms:
+				in_view += 1
 
-	elif ts - timestamps[p] < ms:  # Check if the previous hit object is within the time frame to form a line segment
-		distance = get_distance(coord, coordinates[p])
-		stack = get_stack_obscurity(p, coord, coordinates, ts, timestamps, ms, hitobjects)
+	return in_view
 
-		if (ts - timestamps[p - 1] < ms) and (p != 0):  # Check if the 2nd previous hit object is within the time fram to form a triangle with three points
-			angle = get_angle(p, coord, coordinates)
-			distance_diff = get_distance_diff(p, distance, coordinates)
+def get_raw_circle(line_stats, hitobject_list, map_ms):
+	# Return information about the circle and its raw density
 
-		distance_factor = get_distance_factor(distance)
-		distance_diff_factor = get_distance_diff_factor(distance_diff)
-		angle_factor = get_angle_factor(p, angle, angles)
-		stack_factor = get_stack_factor(stack)
+	# x, y, millisecond timestamp
+	x_coord = line_stats[0]
+	y_coord = line_stats[1]
+	object_timestamp = line_stats[2]
 
-	length_factor = get_length_factor(hitobject_type, line_stats)
+	# Last two attributes are euclidean distance and angle. 0 is a placeholder that will be replaced if necessary.
+	hitobject = circle(x_coord, y_coord, object_timestamp, 0, 0)
+	object_range = get_object_range(hitobject_list, object_timestamp, map_ms)
 
-	if not ('distance_factor' in locals()):
-		distance_factor = get_distance_factor(distance)
-		distance_diff_factor = get_distance_diff_factor(distance_diff)
-		angle_factor = get_angle_factor(p, angle, angles)
-		stack_factor = get_stack_factor(stack)
+	# For if there are no other hitobjects in view before current hitobject
+	if object_range == 0:
+		raw_density = 1  # A lonely circle on the screen should be density 1. right?
+		return hitobject, raw_density
 
-	density = distance_factor * distance_diff_factor * angle_factor * length_factor * stack_factor
+	for i in range(object_range):
 
-	if hitobject_type == 'slider':
-		slider_stats = line_stats[5]
-		slider_stats = slider_stats.split('|')
-		last = (slider_stats[-1]).split(':')
-		coord = np.array(last, dtype=int)
+		x1, y1 = hitobject.x, hitobject.y
 
-		# Will later implement system to replace ts with the timestamp in which the slider ends
+		if i == 1:
+			if type(hitobject_list[-1]) == circle:
+				distance_factor, euclidean_distance = get_distance_factor(x1, y1, hitobject_list[-1].x, hitobject_list[-1].y)
 
-	return hitobject_type, ts, coord, angle, density
+			elif not hitobject_list[-1].movement:
+				# last hitobject is a slider and if slider requires no movement, distance_factor will treat the slider start position as a hit circle, using its start position.
+				distance_factor, euclidean_distance = get_distance_factor(x1, y1, hitobject_list[-1].x_start, hitobject_list[-1].y_start)
 
-def get_density_per_timestamp(timestamps, densities, ms):
-	circle_amount = np.size(timestamps)
-	density_per_timestamp = np.empty(0, dtype=float)
-	for i in range(0, circle_amount):
-		circles_on_screen = 1
-		for j in range(1, circle_amount):
-			try:
-				timestamps[i + j]
-			except:
-				continue
-			if timestamps[i + j] - timestamps[i] < ms:
-				circles_on_screen = circles_on_screen + 1
 			else:
-				break
-		sum = 0
-		for k in range(i, (i + circles_on_screen)):
-			#if k != (circles_on_screen - 1):
-			sum = sum + densities[k]
+				# If last hitobject is a slider that requires movement, distance_factor will be using the end of the slider.
+				distance_factor, euclidean_distance = get_distance_factor(x1, y1, hitobject_list[-1].x_end, hitobject_list[-1].y_end)
+				
+			setattr(hitobject, "euclidean_distance", euclidean_distance)
 
-		density_per_timestamp = np.append(density_per_timestamp, sum)
-	return density_per_timestamp
-
-def get_adjusted_density(map, path_to_map, ms):
-	p = -2  # For indexing purposes.
-
-	hitobjects = np.empty(0, dtype=str)
-	timestamps = np.empty(0, dtype=int)
-	coordinates = np.empty(0, dtype=int)
-	angles = np.empty(0, dtype=float)
-	densities = np.empty(0, dtype=float)
-
-	for line in map:
-		if line.startswith('[HitObjects]'):
-			#  print(line.replace('[HitObjects]', 'e'))
-			break
-
-	for line in map:
-		p += 1  # For indexing purposes, since line is not an integer
-
-		hitobject_type, ts, coord, angle, density = get_adjusted_hitobject(line, p, ms, hitobjects, timestamps, coordinates, angles)
-		if hitobject_type != 'spinner':  # skips every spinner
+		if i == 2:
+				
+			# Used for get_angle_factor
 			
-			hitobjects = np.append(hitobjects, hitobject_type)
-			timestamps = np.append(timestamps, ts)
-			if p > 0:
-				coordinates = np.reshape(np.append(coordinates, coord), newshape=(p + 2, 2))  # add reshape with p
+
+			if type(hitobject_list[-1]) == circle or (type(hitobject_list[-1]) == slider and not hitobject_list[-1].movement):
+				delta_distance_factor = get_delta_distance_factor(hitobject.euclidean_distance, hitobject_list[-1].euclidean_distance)
+
+				if type(hitobject_list[-1]) == circle:
+					x2, y2 = hitobject_list[-1].x, hitobject_list[-1].y
+				else:
+					x2, y2 = hitobject_list[-1].x_start, hitobject_list[-1].y_start
+
+				if type(hitobject_list[-2]) == circle:
+					x3, y3 = hitobject_list[-2].x, hitobject_list[-2].y
+				elif type(hitobject_list[-2]) == slider and not hitobject_list[-1].movement:
+					x3, y3 = hitobject_list[-2].x_start, hitobject_list[-2].y_start
+				else:
+					x3, y3 = hitobject_list[-2].x_end, hitobject_list[-2].y_end
+				
+				angle_factor = get_angle_factor(x1, y1, x2, y2, x3, y3, hitobject_list)
+						
+
 			else:
-				try:
-					coordinates = np.reshape(np.append(coordinates, coord), newshape=(1, 2))
-				except:
-					coordinates = np.reshape(np.append(coordinates, coord), newshape=(2, 2))
+				# placeholders since I don't think this factor matters if the previous hitobject is a slider that requires movement
+				delta_distance_factor = 1
 
+				x2, y2, x3, y3 = hitobject_list[-1].x_end, hitobject_list[-1].y_end, hitobject_list[-1].x_start, hitobject_list[-1].y_start
 
-			angles = np.append(angles, angle)
-			densities = np.append(densities, density)
+				angle_factor = get_angle_factor(x1, y1, x2, y2, x3, y3, hitobject_list)
+
+		# stack factor preparation
+		if type(hitobject_list[i]) == circle:
+			if (x1, y1) == (hitobject_list[i].x, hitobject_list[i].y):
+				stack_ms = object_timestamp - hitobject_list[i].timestamp
+				stack_index = i
+				stack_type = "circle"
 
 		else:
-			p -= 1
- 
-	density_per_timestamp = get_density_per_timestamp(timestamps, densities, ms)
+			if (x1, y1) == (hitobject_list[i].x_end, hitobject_list[i].y_end):
+				stack_ms = object_timestamp - hitobject_list[i].timestamp_end
+				stack_index = i
+				stack_type = "sliderend"
 
-	return np.reshape(np.append(timestamps, density_per_timestamp), newshape=(2, np.size(timestamps)))  # Combines time and density arrays into 2d array.
+			if (x1, y1) == (hitobject_list[i].x_start, hitobject_list[i].y_start):
+				stack_ms = object_timestamp - hitobject_list[i].timestamp_start
+				stack_index = i
+				stack_type = "sliderstart"
+			
+	stack_factor = get_stack_factor(stack_ms, circle, stack_type)
 
-def start_new_map(path_to_map, EZ, HR, DT, HT, adjust):
-	map = open(path_to_map, 'r', encoding='utf-8')
+	density = distance_factor * delta_distance_factor * angle_factor * stack_factor
+	return hitobject, density
+
+
+
+def get_raw_slider(line_stats, hitobject_list, map_ms):
+	# Return information about the slider and its raw density
+	pass
+
+def get_hitobject_type(line_stats):
+	
+	hitobject_num = int(line_stats[3])
+    
+	# Rule out inclusion of spinners in data
+	if hitobject_num == 12:
+		return "spinner"
+	
+	else:
+		if bin(hitobject_num).endswith('1'):
+			return "circle"
+		else:
+			return "slider"
+
+def get_frame_densities(map, map_ms):
+	
+	# First get to where hit objects begin
+	for line in map:
+		if line.startswith('[HitObjects]'):
+			# print(line.replace('[HitObjects]', 'e'))
+			break
+	
+	# The functions that get density will need to access data about previous hitobjects, hence this list
+	hitobject_list = []
+
+	# Hit object data mining begins here
+	for line in map:
+
+		line_stats = line.split
+
+		hitobject_type = get_hitobject_type(line_stats)
+
+		# Exclude spinners from data
+		if hitobject_type == "spinner":
+			continue
+
+		elif hitobject_type == "circle":
+			hitobject, density,  = get_raw_circle(line_stats, hitobject_list, map_ms)
+
+		else:
+			hitobject, density = get_raw_slider(line_stats, hitobject_list, map_ms)
+
+		hitobject_list.append(hitobject)
+
+def start_new_map(file_path, new_ar):
+	map = open(file_path, "r", encoding="utf-8")
 	ar = float(get_original_ar(map))
 	map.seek(0)  # Resets line variable for next instance of 'for line in map'
 
-	if EZ == 1 or HR == 1 or DT == 1 or HT == 1:
-		ar, ms = get_modded_stats(ar, EZ, HR, DT, HT)
-	else:
-		ms = ar_to_ms(ar)  # Gets the "ms" value, in case there are no mods enabled
-	if adjust == 0:
-		density_data = (get_raw_density(map, path_to_map, ms))
-	
-	else:
-		density_data = (get_adjusted_density(map, path_to_map, ms))
-	return density_data
+	if new_ar == "EZ":
+		# Easy (EZ) mod cuts AR in half
+		ar = ar * 0.5
+
+	elif new_ar == "HR":
+		# Hardrock (HR) mod multiplies AR by 1.4 up to a maximum of 10
+		if (ar * 1.4) > 10:
+			ar = 10
+		else:
+			ar = ar * 1.4
+
+	elif new_ar != "NM":
+		# For if user wishes to simulate readability with a new AR.
+		# new_ar must be within the AR range zero to 11
+		if (0 <= float(new_ar) <= 11):
+			ar = new_ar
+		# When making GUI, add blockage that simulates this requirement
+
+		map_ms = ar_to_ms(ar)
+
+	frame_densities = get_frame_densities(map, map_ms)
